169a4130c4a7e7a35b10e89effba981b4910833d
Samuel Hufen <samuel@hufen.xyz>
2021-11-27 16:32:41 +0100

    Removed unused using directives to allow a new commit to test the github actions.

--- KernelCloudPlugin/KernelCloudPlugin.cs
+++ KernelCloudPlugin/KernelCloudPlugin.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Lumos.GUI;
+﻿using Lumos.GUI;
 using Lumos.GUI.Plugin;
 using LumosLIB.Kernel.Log;
 
